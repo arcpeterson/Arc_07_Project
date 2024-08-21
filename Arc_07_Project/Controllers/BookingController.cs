f035f0739f21feb69310f7c407e4692b514b6d81
arc <arc@MSI>
2024-08-21 09:26:51 +0800

    Add project files.

---
+++ Arc_07_Project/Controllers/BookingController.cs
@@ -0,0 +1,74 @@
+﻿using Arc_07_Project.Data;
+using Arc_07_Project.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arc_07_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Bookings);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int? id)
+        {
+            var bookings = _context.Bookings.FirstOrDefault(e => e.BookingID == id);
+            if (bookings == null)
+                return Problem(detail: "Booking with id " + id + " is not found. ", statusCode: 404);
+            return Ok(bookings);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Booking booking)
+        {
+            _context.Bookings.Add(booking);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetAll", new { id = booking.BookingID }, booking);
+        }
+
+        [HttpPut]
+        public IActionResult Put(int? id, Booking booking)
+        {
+            var entity = _context.Bookings.FirstOrDefault(e => e.BookingID == id);
+            if (entity == null)
+                return Problem(detail: "Booking with id " + id + " is not found ", statusCode: 404);
+
+            entity.FacilityDescription = booking.FacilityDescription;
+            entity.BookingDateFrom = booking.BookingDateFrom;
+            entity.BookingDateTo = booking.BookingDateTo;
+            entity.BookedBy = booking.BookedBy;
+            entity.BookingStatus = booking.BookingStatus;
+
+            _context.SaveChanges();
+
+            return Ok(entity);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id, Booking booking)
+        {
+            var entity = _context.Bookings.FirstOrDefault(e => e.BookingID == id);
+            if (entity == null)
+                return Problem(detail: "Booking with id " + id + " is not found ", statusCode: 404);
+
+            _context.Bookings.Remove(entity);
+            _context.SaveChanges();
+
+            return Ok(entity);
+        }
+    }
+}
---
+++ Arc_07_Project/Models/RegisterModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Arc_07_Project.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "User Name is required.")]
+        public string? Username { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is reuiqred.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string? Password { get; set; }
+    }
+}
